2694992c4315fc622390785f718f262f8dfbc553
thomas peyregne <thomas.peyregne@live.fr>
2015-11-05 23:43:37 +0100

    Base

---
+++ TAP/TAP/TAP/Assets/scripts/modules/core/module.ts
@@ -0,0 +1,8 @@
+﻿/// <reference path="../../../../typings/tsd.d.ts" />
+
+module TAP.Core {
+    'uses strict';
+
+    angular.module("TAP.Core", ["TAP.Core.Controllers", "TAP.Core.Services"]);
+
+}
